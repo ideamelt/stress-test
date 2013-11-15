aea32e413424f8f7d0f18c85490715a94af37a5f
alizainf <alizain@ideamelt.com>
2013-11-15 15:47:43 -0500

    m

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-stress-test
-===========
+# Echo Stress Test
+
+This module simulates real-world load for IdeaMelt's Notifier app.
+
+## Instructions
+
+To run this test, simply run the following command in shell
+
+`python search_test.py`
+
+## Dependencies
+
+`grequests`
+`libevent`
+
+## Configuration
+
+To change the scope of the test, change the following parameters in the `settings.py` file
+
+`SEARCH_TOTAL_TIME = 20 # seconds`
+`SEARCH_INTERVAL_TIME = 5 # seconds`
+`USERS_TO_TEST = 5 # you can stress test a random sample users (must be less than 1000)`
+
+## More
+
+Errors are printed out to the console and stored in the log folder
+
+Full JSON data output of the test results are stored in the data folder
---
+++ helpers.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+import urllib
+import settings
+import simplejson as json
+
+# common methods
+def create_echo_url(user_url_s):
+    return (settings.ECHO_API_URL + settings.ECHO_CLIENT_URL + settings.ECHO_DATATYPE_URL +
+            urllib.quote_plus(user_url_s) + settings.ECHO_APP_KEY_QP)
+
+def export_data(filename, data):
+    with open('data/%s-%s.json' % (filename, datetime.utcnow()), 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+def aggregate_data(global_data):
+	global_data['stats'] = {}
+	# global stats
+	total_requests = 0
+	total_request_time = 0
+	# iterate over each batch of tests
+	for interval in global_data['raw']:
+		data = global_data['raw'][interval]
+		count = len(data)
+		total = 0
+		# aggregate time
+		for user in data:
+			total = total + data[user]
+		# save data to global
+		total_requests += count
+		total_request_time += total
+		# save data
+		global_data['stats'][interval] = {
+			'sent': count,
+			'total': round(total / 1000, 6),
+			'average': round((total / count) / 1000, 6),
+		}
+	# calculate global numbers
+	global_data['total_requests'] = total_requests
+	global_data['avg_request_time'] = round((total_request_time / total_requests) / 1000, 6)
+	# return it all back!
+	return global_data
+
+
+
+
