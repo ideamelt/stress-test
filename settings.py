8bedd73af2fdffbe26fc4a0d9ca139fd8b7f0d77
alizainf <alizain@ideamelt.com>
2013-11-15 16:20:11 -0500

    m

--- settings.py
+++ settings.py
@@ -12,6 +12,6 @@ USER_MAX = 1000 # Max number of testable users (DO NOT CHANGE THIS VALUE)
 USER_LIST = range(1, USER_MAX + 1)
 
 # Stress test settings
-SEARCH_TOTAL_TIME = 20 # seconds
-SEARCH_INTERVAL_TIME = 2 # seconds
-USERS_TO_TEST = 5 # you can stress test a random sample users set in USER_MAX (must be less than USER_MAX)
+SEARCH_TOTAL_TIME = 60 # (seconds) - how long a user is expected to stay on the site
+SEARCH_INTERVAL_TIME = 5 # (seconds) - liveUpdatesInterval
+USERS_TO_TEST = 20 # the script will test a random sample of users (must be less than 1000)
